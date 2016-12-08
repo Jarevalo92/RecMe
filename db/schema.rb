ba3391ecf2826932e7e2aa896e5c1aba8fa0b2a9
John Arevalo <john.arevalo11@gmail.com>
2016-12-07 19:05:21 -0500

    fixed gemfile & removed duplicate migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20161207223243) do
-
-ActiveRecord::Schema.define(version: 20161207210850) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "sport"
